1b1b5750f73c8e51f16af5ee2fbd6981f631ea0c
U NAVEEN KUMAR <uppalanaveenkumar1@gmail.com>
2026-06-11 07:33:24 +0530

    06-11-2026 0053-maximum-subarray

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cursum=nums[0];
+        int maxsum=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+        cursum=Math.max(nums[i],cursum+nums[i]);
+        maxsum=Math.max(maxsum,cursum);
+        //  currSum = Math.max(nums[i], currSum + nums[i]);
+        // maxSum = Math.max(maxSum, currSum);
+        
+        }
+        return maxsum;
+        }
+}
