ec01f26f8dcea6727ea6b8487d950a73b05f3f4b
U NAVEEN KUMAR <uppalanaveenkumar1@gmail.com>
2026-06-18 06:48:05 +0530

    06-18-2026 0637-average-of-levels-in-binary-tree

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            double sum = 0;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+                sum += current.val;
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+            result.add(sum / levelSize);
+        }
+
+        return result;
+    }
+}
